fb8e299144751bf00500f56b31ecb438616c8f60
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-01-04 22:46:24 +0200

    feat(blockchain): implement proof of work function

--- blockchain.py
+++ blockchain.py
@@ -52,6 +52,23 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+        - Find a number p' such as hash(pp') contains leading 4 zeroes
+          where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False
+            proof += 1
+
+        return proof
+
     @property
     def last_block(self):
         return self.chain[-1]
