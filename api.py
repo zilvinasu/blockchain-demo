b970802e4e7ea5b520f5c5abfcabfe39dc3b2675
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-01-04 21:58:10 +0200

    feat(api): init endpoints

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from flask import Flask
+from flask import Flask, jsonify
 
 from blockchain import Blockchain
 
@@ -11,3 +11,26 @@ node_identifier = str(uuid4()).replace('-', '')
 
 # Init the Blockchain
 blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
