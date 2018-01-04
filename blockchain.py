0ba698f92b77242d75238475cfbb332711af0a4f
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-01-04 21:10:33 +0200

    feat: implement new block creation

--- blockchain.py
+++ blockchain.py
@@ -8,9 +8,28 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+
+        :param proof: <int> The proof given by The Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of the previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
